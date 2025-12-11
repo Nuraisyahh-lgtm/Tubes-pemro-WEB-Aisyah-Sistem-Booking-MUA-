ad63049fcc96fdea852d7e034ff5f39c68e14501
Nuraisyahh-lgtm <aisyahhnurman@gmail.com>
2025-12-11 10:24:54 +0800

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "db_mua");
-
-// Error Handling (Syarat Tubes)
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
-?>
